6a7dfd4dc9d6c96caf22022a37f131bc3d5bffc0
Jmeel <46560046+Jmeel@users.noreply.github.com>
2019-01-30 21:02:40 +0200

    Add files via upload

---
+++ k.py
@@ -0,0 +1,95 @@
+
+
+
+
+from os import *
+	
+system("pkg upgrade -y")
+
+system("pkg update -y")
+
+system("pkg update")
+
+system("pkg upgrade")
+
+system("pkg install python")
+
+system("pkg install python2")
+
+system("pkg install python3")
+
+system("pkg install ruby")
+
+system("pkg install git")
+
+system("pkg install php")
+
+system("pkg install java")
+
+system("pkg install google")
+
+system("pkg install bash")
+
+system("pkg install perl")
+
+system("pkg install nmap")
+
+system("pkg install bash")
+
+system("pkg install clang")
+
+system("pkg install macchanger")
+
+system("pkg install nano")
+
+system("pkg install figlet")
+
+system("pkg install cowsay")
+
+system("pkg install curl")
+
+system("pkg install tar")
+
+system("pkg install zip")
+
+system("pkg install unzip")
+
+system("pkg install tor")
+
+system("pkg install sudo")
+
+system("pkg install wget")
+
+system("pkg install wcalc")
+
+system("pkg install openssl")
+
+system("pkg install bmon")
+
+system("apt install git")
+
+system("apt install perl")
+
+
+system("pkg install sl")
+
+system("pkg install neofetch")
+
+system("pkg install cmatrix")
+
+system("pkg install tty-clock")
+
+system("pkg install curseofwar")
+
+system("clear")
+
+print("  All Termix application commands have been successfully installed")
+
+print("|×××××××××××|"*70)
+
+print("By Jmeel Ahmad")
+
+print("|×J×m×e×e×l×|"*70)
+	
+
+
